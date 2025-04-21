a5f66efa084e7fc865411c5e61716faec9749fcf
joceesagar <joshisagarm2@gmail.com>
2025-04-21 09:26:22 +0545

    setup stack and tab router and gathered assets

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-dark-100 font-bold">Hello World!</Text>
+    </View>
+  );
+}
+
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,18 @@ import { Stack } from "expo-router";
 import './globals.css'
 
 export default function RootLayout() {
-  return <Stack />;
+  //This is stack navigation for navigating between (tabs) and movie
+  return <Stack>
+    {/* 👇 This sets up the (tabs) folder as the main screen group using bottom tab navigation */}
+    <Stack.Screen
+      name="(tabs)"
+      options={{ headerShown: false }}
+    />
+
+    {/* 👇 This adds another screen to the stack for movie detail pages (navigated from tabs) */}
+    <Stack.Screen
+      name="movie/[id]"
+      options={{ headerShown: false }}
+    />
+  </Stack>;
 }
