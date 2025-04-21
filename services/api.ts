cec810d7747aff8da0d44a15286ff266603c6e6b
joceesagar <joshisagarm2@gmail.com>
2025-04-21 16:19:37 +0545

    configured TMDB api

---
+++ services/api.ts
@@ -0,0 +1,28 @@
+export const TMDB_CONFIG = {
+    BASE_URL: 'https://api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
+    }
+}
+
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` //fetch movies according to user search. encodedURIComponent is for not escaping special characters
+        : `${TMDB_CONFIG}/discover/movie?sort_by=popularity.desc`; //fetch most popular movies
+
+
+    const response = await fetch(endpoint, {
+        method: 'GET',
+        headers: TMDB_CONFIG.headers
+    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch movies')
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
